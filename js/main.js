ad1be1d15e16faa3d6643e3a56a1ec98df1ae0ee
Stawerr <renato-silva_16@hotmail.com>
2022-02-07 14:23:55 +0000

    first

---
+++ js/main.js
@@ -0,0 +1,33 @@
+var toastLiveExample = document.getElementById('liveToast')
+document.getElementById('liveToastBtn1').addEventListener('click', function () {
+    var toast = new bootstrap.Toast(toastLiveExample)
+    toast.show()
+  })
+document.getElementById('liveToastBtn2').addEventListener('click', function () {
+    var toast = new bootstrap.Toast(toastLiveExample)
+    toast.show()
+  })
+  document.getElementById('liveToastBtn3').addEventListener('click', function () {
+    var toast = new bootstrap.Toast(toastLiveExample)
+    toast.show()
+  })
+  document.getElementById('liveToastBtn4').addEventListener('click', function () {
+    var toast = new bootstrap.Toast(toastLiveExample)
+    toast.show()
+  })
+  document.getElementById('liveToastBtn5').addEventListener('click', function () {
+    var toast = new bootstrap.Toast(toastLiveExample)
+    toast.show()
+  })
+  document.getElementById('liveToastBtn6').addEventListener('click', function () {
+    var toast = new bootstrap.Toast(toastLiveExample)
+    toast.show()
+  })
+  document.getElementById('liveToastBtn7').addEventListener('click', function () {
+    var toast = new bootstrap.Toast(toastLiveExample)
+    toast.show()
+  })
+  document.getElementById('liveToastBtn8').addEventListener('click', function () {
+    var toast = new bootstrap.Toast(toastLiveExample)
+    toast.show()
+  })
